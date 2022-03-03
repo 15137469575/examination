91feff14e5f9e496b62a721bc229382ff4a90db7
15137469575 <948079054@qq.com>
2022-03-03 20:57:35 +0800

    项目基本文件结构

---
+++ examination/src/test/java/org/upc/examination/ExaminationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.upc.examination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
