2dc621271617645daf644ac77cd8e55b64706012
siyongfei <3137753170@qq.com>
2022-03-09 21:17:32 +0800

    第四次

---
+++ examination/src/main/resources/static/js/examPrepare.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+	var back = document.getElementById('back');
+	var start = document.getElementById('start');
+
+	back.onclick = function() {
+		location.href = 'student_02.html';
+	}
+	start.onclick = function() {
+		location.href = 'paper.html';
+	}
+
+}
