f3ce1f9de81588b6162be9f79ec45169f4264f24
yueyuanjian <3157908113@qq.com>
2022-03-20 18:44:00 +0800

    yyj

--- examination/src/main/java/org/upc/examination/mapper/PaperInformationMapper.java
+++ examination/src/main/java/org/upc/examination/mapper/PaperInformationMapper.java
@@ -3,8 +3,16 @@ package org.upc.examination.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.upc.examination.entity.PaperInformation;
 
+import java.util.List;
+
 @Mapper
 public interface PaperInformationMapper {
 
     int insert(PaperInformation paperInformation);
+
+     List<PaperInformation> selectById(int index);
+
+     PaperInformation selectBySubjects(String subject);
+
+
 }
