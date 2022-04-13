0bdff9d8214d7c485fd9d708b7d3290fa31d636f
15137469575 <948079054@qq.com>
2022-04-13 19:29:53 +0800

    4月13日提交二

--- examination/src/main/java/org/upc/examination/controller/FaceController.java
+++ examination/src/main/java/org/upc/examination/controller/FaceController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.upc.examination.service.UserFaceInfoService;
 
-
 @RestController
 @RequestMapping("/face")
 public class FaceController {
